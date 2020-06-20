7adb95e847cb109317e0722117bf7dbd44f7982a
shunjid <shunjid1862@gmail.com>
2020-06-20 15:02:59 +0600

    init hackerrank

---
+++ cpp-input-and-output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    short a, b, c;  
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
